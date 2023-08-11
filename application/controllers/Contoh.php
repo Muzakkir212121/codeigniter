c48809ba2957d97936ba9765a3d0c316e319a88e
Muzakkir <52663583+Muzakkir212121@users.noreply.github.com>
2023-08-11 22:22:07 +0700

    first commit

---
+++ application/controllers/Contoh.php
@@ -0,0 +1,11 @@
+<?php
+
+class Contoh extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h1>Hellow
+		</h1>";
+	}
+}
+?>
---
+++ application/views/contoh_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh View</title>
+</head>
+<body>
+    <h1>Contoh View pada Codeigneiter</h1>
+    <hr>
+    <p>Belajar Codeneighter di Framwork Indonesia</p>
+</body>
+</html>
